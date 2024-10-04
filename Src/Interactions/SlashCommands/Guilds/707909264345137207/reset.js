383d63c50c301a10c0d2e20d33729bca15761478
regenmantel <philipstedronsky@gmail.com>
2024-10-04 17:36:24 +0200

    updated reset cmd to active db

--- Src/Interactions/SlashCommands/Guilds/707909264345137207/reset.js
+++ Src/Interactions/SlashCommands/Guilds/707909264345137207/reset.js
@@ -1,12 +1,12 @@
-const { conn } = require('../../../../functions/conn');
-const config = require('../../../../Credentials/Config');
+const { conn } = require("../../../../functions/conn");
+const config = require("../../../../Credentials/Config");
 
 module.exports = {
-	name: 'reset',
-	description: 'Reset Hall of Bash',
+	name: "reset",
+	description: "Reset Hall of Bash",
 	onlyRoles: [config.server.roles.sf, config.server.roles.mod],
 	run: async (interaction) => {
-		let tableName = 'de221';
+		let tableName = "de233";
 
 		await conn(`UPDATE \`${tableName}\` SET bashPoints = 0;`);
 		await interaction.reply({ content: `Hall of Bash wurde zurückgesetzt.`, ephemeral: true });
